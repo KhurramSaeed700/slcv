1977e69c76ff2b159a719c8fd9e13976a2bcda87
Abdul Wahab <80677064+599-wahab@users.noreply.github.com>
2024-01-01 10:16:45 +0500

    Admin Page UI changes

--- lib/pages/AdminPage.dart
+++ lib/pages/AdminPage.dart
@@ -5,45 +5,167 @@ class AdminPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text("Admin Dashboard"),
         backgroundColor: Colors.blue[500],
         elevation: 23,
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.logout))
+          IconButton(onPressed: () {}, icon: const Icon(Icons.logout)),
         ],
       ),
-      drawer:   Drawer(
-          backgroundColor: Colors.white,
-          child:  Column(
-            children: [
-              Image.asset(
-              'lib/assets/main-logo.png'
-            ),
-              const TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search',
-                  prefixIcon: Icon(Icons.search),
-                ),
+      drawer: Drawer(
+        backgroundColor: Colors.white,
+        child: Column(
+          children: [
+            Image.asset('lib/assets/main-logo.png'),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: 'Search',
+                prefixIcon: Icon(Icons.search),
               ),
+            ),
             const TextField(
               decoration: InputDecoration(
                 hintText: 'Search by Image',
                 suffixIcon: Icon(Icons.camera_alt_outlined),
               ),
             ),
-              const TextField(
-                decoration: InputDecoration(
-                  hintText: 'Add camera',
-                  suffixIcon: Icon(Icons.add_a_photo_outlined),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: 'Add camera',
+                suffixIcon: Icon(Icons.add_a_photo_outlined),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            // Left Column - Cameras
+            Expanded(
+              flex: 6,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                elevation: 4,
+                child: const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Cameras',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      // List of Camera Containers goes here
+                      // You can dynamically add new containers in this area
+                    ],
+                  ),
                 ),
               ),
+            ),
+            // Right Column - Logs/Entries
+            Expanded(
+              flex: 3,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                elevation: 4,
+                color: Colors.blue[500],
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Expanded(
+                        flex: 8,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Logs / Entries',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            // Content for the top 80% goes here
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: Center(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Text(
+                                      'Authorized ',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 10,
+                                      height: 10,
+                                      decoration: const BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Colors.green,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                                child: Center(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      const Text(
+                                        'Unauthorized ',
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Container(
+                                        width: 10,
+                                        height: 10,
+                                        decoration: const BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          color: Colors.red,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
-          ),
         ),
-
+      ),
     );
   }
-
 }
